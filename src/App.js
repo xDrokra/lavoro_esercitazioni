383256f5e1a5932d84f6dd9129c5d13cf49417ff
Leonardo <leocolgucci@gmail.com>
2025-09-22 07:32:26 +0200

    Modificato input aggiungendo label, icona, e validazione di esso, piu' documentazione

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import Card from "./components/Card"
 import Table from "./components/Table";
 import { useState } from "react";
 
+
+
+
 function App() {
   // Stato modale
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -57,9 +60,37 @@ function App() {
       <hr></hr>
 
       {/* Esempio input */}
-      <div className="p-5">
-        <h1 className="text-3xl mb-3 font-bold">Esempio input</h1>
-        <Input placeHolder="placeholder" className="block" />
+      <div className="p-5 flex flex-col gap-4">
+        <h1 className="text-3xl font-bold">Esempio input</h1>
+        <Input
+          label="Email"
+          type="email"
+          placeholder="Email"
+        />
+
+        <Input
+          placeholder="Cerca"
+          icon={<svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="20" height="20" viewBox="0 0 50 50">
+            <path d="M 21 3 C 11.621094 3 4 10.621094 4 20 C 4 29.378906 11.621094 37 21 37 C 24.710938 37 28.140625 35.804688 30.9375 33.78125 L 44.09375 46.90625 L 46.90625 44.09375 L 33.90625 31.0625 C 36.460938 28.085938 38 24.222656 38 20 C 38 10.621094 30.378906 3 21 3 Z M 21 5 C 29.296875 5 36 11.703125 36 20 C 36 28.296875 29.296875 35 21 35 C 12.703125 35 6 28.296875 6 20 C 6 11.703125 12.703125 5 21 5 Z"></path>
+          </svg>}
+        />
+
+        <Input
+          label="Password"
+          type="password"
+          placeholder="password"
+          value="leomalito@gmail.com"
+          variant="error"
+          errorMessage="Password errata"
+        />
+
+        <Input
+          label="Email"
+          type="email"
+          placeholder="email"
+          value="leomalito@gmail.com"
+          variant="success"
+        />
       </div>
 
       <hr></hr>
--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -6,24 +6,52 @@
  *
  * @param {Object} props proprieta' del componente
  * @param {"text" | "password" | "email" | "number" | string} [props.type="text"] tipo di input
- * @param {"primary" | string} [props.variant="primary"] lo stile variante dell'input
+ * @param {"primary" | "error" | "success" | string} [props.variant="primary"] lo stile variante dell'input
+ * @param {string} [props.label] etichetta opzionale per l'input
+ * @param {string} [props.errorMessage] messaggio di errore opzionale da visualizzare sotto l'input
+ * @param {React.ReactNode} [props.icon] icona opzionale da visualizzare all'interno dell'input
  * @param {string} [props.className=""] classi aggiuntive per la personalizzazione
  * @param {any} [props.props] props HTML dell'input
  *
  */
 
-const Input = ({ type = 'text', variant = 'primary', className = '', ...props }) => {
-    const defaultStyle = 'px-3 py-2'
+const Input = ({ type = 'text', label, variant = 'primary', errorMessage, className = '', icon, ...props }) => {
+    const defaultStyle = 'px-3 py-2 border-2 rounded-lg'
     const variants = {
-        primary: 'border-2 rounded-lg focus:outline-none'
+        primary: '',
+        error: "border-red-500 focus:border-red-600",
+        success: "border-green-500 focus:border-green-600",
     }
 
     return (
-        <input
-            type={type}
-            className={`${defaultStyle} ${variants[variant]} ${className}`}
-            {...props}
-        />
+
+        <div>
+
+            {label && <label className="block mb-1 font-medium">{label}</label>}
+
+            {icon ? (
+                <div className={`flex items-center ${defaultStyle} ${className} ${variants[variant]} max-w-sm focus-within:border-black`}>
+                    {icon && <span>{icon}</span>}
+                    <input
+                        type={type}
+                        className={`pl-3 bg-transparent border-none focus:outline-none`}
+                        {...props}
+                    />
+                </div>
+            ) : (
+
+                <input
+                    type={type}
+                    className={`${defaultStyle} ${variants[variant]} ${className}`}
+                    {...props}
+                />
+            )}
+
+
+
+
+            {errorMessage && <p className="text-red-500 text-sm mt-1">{errorMessage}</p>}
+        </div>
     )
 }
 
