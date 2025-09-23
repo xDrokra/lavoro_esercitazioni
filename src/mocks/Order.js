f99c922dd0820a8a0388fd203ccf7e4f56a79730
Leonardo <leocolgucci@gmail.com>
2025-09-23 02:52:27 +0200

    Aggiunti mocks di esempi

---
+++ src/mocks/Order.js
@@ -0,0 +1,41 @@
+// Data di esempio per la tabella degli ordini
+const Order = [
+    {
+        "Documento": "OV-23-U/11234",
+        "vs. riferimento": "Giovanni Mirenda",
+        "data": "2023-12-10",
+        "stato": "tracking"
+    },
+    {
+        "Documento": "OV-24-U/11234",
+        "vs. riferimento": "Giorgio Triusciuzzi",
+        "data": "2023-10-11",
+        "stato": "tracking"
+    },
+    {
+        "Documento": "OV-25-U/11234",
+        "vs. riferimento": "Luca Bianchi",
+        "data": "2023-10-12",
+        "stato": "tracking"
+    },
+    {
+        "Documento": "OV-26-U/11234",
+        "vs. riferimento": "Marco Rossi",
+        "data": "2023-10-13",
+        "stato": "tracking"
+    },
+    {
+        "Documento": "OV-27-U/11234",
+        "vs. riferimento": "Paolo Verdi",
+        "data": "2023-10-14",
+        "stato": "tracking"
+    },
+    {
+        "Documento": "OV-28-U/11234",
+        "vs. riferimento": "Andrea Neri",
+        "data": "2023-10-15",
+        "stato": "tracking"
+    },
+];
+
+export default Order;
---
+++ src/mocks/Utenti.js
@@ -0,0 +1,18 @@
+
+// Data di esempio per la tabella degli utenti
+const Utenti = [
+    {
+        "Nome": "Mario Rossi",
+        "Email": "mario.rossi@example.com"
+    },
+    {
+        "Nome": "Luigi Verdi",
+        "Email": "luigi.verdi@example.com"
+    },
+    {
+        "Nome": "Giovanni Bianchi",
+        "Email": "giovanni.bianchi@example.com"
+    }
+];
+
+export default Utenti;
